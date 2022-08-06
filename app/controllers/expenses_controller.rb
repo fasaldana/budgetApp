9e74ec196137b62c86227bdbb97240bfbc9238aa
fasaldana <fernando_s_98@hotmail.com>
2022-08-06 18:10:59 -0500

    Fix linters

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ExpensesController < ApplicationController
   before_action :set_group, only: %i[new create edit update destroy]
   before_action :set_expense, only: %i[edit update destroy]
